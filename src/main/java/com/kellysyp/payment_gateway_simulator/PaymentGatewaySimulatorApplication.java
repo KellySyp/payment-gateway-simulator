53502ffc45677ef881d6235b945ebf63efb6f687
Kelly <kelmsyp@gmail.com>
2026-01-21 15:28:48 -0500

    Initial commit

---
+++ src/main/java/com/kellysyp/payment_gateway_simulator/PaymentGatewaySimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.kellysyp.payment_gateway_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGatewaySimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentGatewaySimulatorApplication.class, args);
+	}
+
+}
